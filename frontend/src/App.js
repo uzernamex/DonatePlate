c2b544fa15975c238161b3f4ab6a841073a79669
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2024-01-31 20:12:13 -0700

    Add CSS for login and logout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
-
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading, error } = useAuth0();
   return (
     <main >
-      <h1>Auth0 Login</h1>
+      <h1>Welcome to DonatePlate</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
       <LoginButton/>
       <LogoutButton/>
+      <Profile/>
+      </>
+      )}
     </main>
   );
 }
