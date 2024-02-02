3cfea12bfcf40e1c00ea8200c0f8311ecde1a687
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2024-02-01 20:12:26 -0500

    Update db queries to pull information

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -1,10 +1,26 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
 };
 
-module.exports = { getUsers };
+const getFoodDonations = () => {
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
+};
+const getAddress = () => {
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
+};
+
+const getMessages = () => {
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { getUsers, getAddress, getMessages, getFoodDonations };
