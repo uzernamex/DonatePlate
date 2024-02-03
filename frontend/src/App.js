f84d1cb79f91fcbebeb048eb59491078fd635085
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2024-02-03 16:30:55 -0500

    Delete console logs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
-import FoodDonationForm from "./components/FoodDonationForm"
-import AddressForm from "./components/AddressForm"
+import FoodDonationForm from "./components/FoodDonationForm";
+import AddressForm from "./components/AddressForm";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
@@ -17,11 +17,10 @@ function App() {
       {!error && isLoading && <p>Loading...</p>}
       {!error && !isLoading && user && (
         <>
-          
           <LogoutButton />
           <Profile />
-          <FoodDonationForm/>
-          <AddressForm/>
+          <FoodDonationForm />
+          <AddressForm />
         </>
       )}
     </main>
