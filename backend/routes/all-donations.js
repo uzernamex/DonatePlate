1a3abda430dafb8a9a9fa58d783d2551596bc482
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2024-02-07 15:46:26 -0500

    Add enpoint for donation form

--- backend/routes/all-donations.js
+++ backend/routes/all-donations.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userQueries = require("../db/queries/data_queries");
 
-router.post("/", (req, res) => {
+router.get("/all-donations", (req, res) => {
   const formData = req.body;
   console.log(formData);
 });
