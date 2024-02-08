7f8df3a766f7ab83319ea0a61b5b0d0352733de9
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2024-02-07 18:31:31 -0700

    Remove consoles

---
+++ frontend/src/components/MessageForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Formik } from "formik";
+
+
+const MessageForm = () => (
+  <div className="message-form-container">
+    <h1> Contact Us</h1>
+    <Formik>
+    </Formik>
+  </div>
+);
+
+export default MessageForm;
