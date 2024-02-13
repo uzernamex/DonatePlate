a8a7eff5a91147d53a7ffa12643f9e2cd83e0a58
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2024-02-12 19:50:18 -0700

    Removed unwanted components from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import Profile from "./components/Profile";
-import FoodDonationForm from "./components/FoodDonationForm";
-import DisplaySingleDonationRequest from "./components/DisplaySingleDonationRequest";
-import InsertMessageForm from "./components/InsertMessageForm";
-import DisplayAllMessages from "./components/DisplayAllMessages";
+
 
 import { useAuth0 } from "@auth0/auth0-react";
 import FoodDonationsCard from './components/AllDonationsPage';
@@ -20,9 +17,6 @@ function App() {
       {!error && !isLoading && user && (
         <>
           <Profile />
-          <DisplaySingleDonationRequest />
-          <InsertMessageForm />
-          <DisplayAllMessages />
           <div className="form-container">
             {/* <FoodDonationForm /> */}
             <FoodDonationsCard />
