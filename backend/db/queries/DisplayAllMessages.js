c3afa76efcc7653b026d7a8d1aa6a82dbe7955e5
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2024-02-14 14:07:52 -0700

    Remove unwanted files

--- backend/db/queries/DisplayAllMessages.js
+++ backend/db/queries/DisplayAllMessages.js
@@ -2,7 +2,8 @@ const db = require('../connection');
 
 const displayAllMessages = () => {
   return db.query(
-    'SELECT name, email, message FROM messages'
+    //'SELECT name, email, message FROM messages'
+    'SELECT messages.*, food_donations.user_id FROM messages JOIN food_donations ON messages.food_donation_id = food_donations.id'
   )
     .then((result) => {
       const { rows } = result;
